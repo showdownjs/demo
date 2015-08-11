40d6b717a31ad95d0328b56df10ffb9db4d0896d
Estevão Soares dos Santos <estevao.santos@gmail.com>
2015-08-11 05:39:18 +0100

    add slide menu with options

--- js/editor.js
+++ js/editor.js
@@ -1,22 +1,91 @@
-var app = angular.module('showdown.editor', ['ng-showdown']);
+var app = angular.module('showdown.editor', ['ng-showdown', 'pageslide-directive', 'ngAnimate']);
 
-app.config(['$showdownProvider', function ($showdownProvider) {
-  $showdownProvider.setOption('omitExtraWLInCodeBlocks',   true);
-  $showdownProvider.setOption('simplifiedAutoLink',        true);
-  $showdownProvider.setOption('literalMidWordUnderscores', true);
-  $showdownProvider.setOption('strikethrough',             true);
-  $showdownProvider.setOption('tables',                    true);
-  $showdownProvider.setOption('tablesHeaderId',            true);
-  $showdownProvider.setOption('ghCodeBlocks',              true);
-  $showdownProvider.setOption('tasklists',                 true);
-  $showdownProvider.setOption('parseImgDimensions',        true);
-  $showdownProvider.setOption('parseImgDimensions',        true);
-  $showdownProvider.setOption('headerLevelStart',          3);
-  $showdownProvider.setOption('smoothLivePreview',         true);
+app.config([function () {
+
 }]);
 
+app.directive('squeeze', ['$animate', function($animate){
+
+  return {
+    link: function(scope, element, attrs) {
+      scope.$watch('checked', function(newValue) {
+        if (!scope.firstLoad) {
+          if (newValue) {
+            $animate.addClass(element, 'squeezed-body');
+            element.removeClass('full-body');
+          } else {
+            $animate.addClass(element, 'full-body');
+            element.removeClass('squeezed-body');
+          }
+        }
+      });
+    }
+  };
+
+  /*
+  return function(scope, element, attrs) {
+    var width = (scope.checked) ? $window.innerWidth - 300 : $window.innerWidth;
+    width += 'px';
+    var body = angular.element(document.body);
+    document.body.style.width = width;
+    console.log(scope);
+     if (scope.checked) {
+     $animate.addClass(element, 'squeezed-body');
+     } else {
+     $animate.removeClass(element, 'squeezed-body')
+     }
+  };
+  */
+}]);
+
+
 app.controller('editorCtrl', ['$scope', '$showdown', '$http', function($scope, $showdown, $http) {
+  $scope.checked = false;
+  $scope.firstLoad = true;
+
+  $scope.toggleMenu = function () {
+    $scope.firstLoad = false;
+    $scope.checked = !$scope.checked;
+  };
+
   $scope.text = '';
+
+  /** options **/
+  $scope.checkOpts = [
+    { name: 'omitExtraWLInCodeBlocks', value: true },
+    { name: 'noHeaderId',              value: false },
+    { name: 'parseImgDimensions',      value: true },
+    { name: 'simplifiedAutoLink', value: true },
+    { name: 'literalMidWordUnderscores', value: true },
+    { name: 'strikethrough', value: true },
+    { name: 'tables', value: true },
+    { name: 'tablesHeaderId', value: false },
+    { name: 'ghCodeBlocks', value: true },
+    { name: 'tasklists', value: true },
+    { name: 'smoothLivePreview', value: true },
+    { name: 'prefixHeaderId', value: false }
+  ];
+  $scope.valOpts = [
+    { name: 'headerLevelStart', value: 3 }
+  ];
+  /** end options **/
+
+  $scope.updateOptions = function () {
+    for ( var i = 0; i < $scope.checkOpts.length; ++i) {
+      $showdown.setOption($scope.checkOpts[i].name, $scope.checkOpts[i].value);
+    }
+
+    for ( i = 0; i < $scope.valOpts.length; ++i) {
+      $showdown.setOption($scope.valOpts[i].name, $scope.valOpts[i].value);
+    }
+
+    // trigger a showdown reload
+    $scope.text = $scope.text + '&';
+    $scope.text.replace(/&$/, '');
+
+  };
+
+  $scope.updateOptions();
 
   $http.get('md/text.md')
     .success(function(data) {
@@ -25,4 +94,6 @@ app.controller('editorCtrl', ['$scope', '$showdown', '$http', function($scope, $
     .error(function() {
       $scope.text =  "Request failed";
     });
+
+
 }]);
