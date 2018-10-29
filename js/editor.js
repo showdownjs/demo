11f48d4e47d8c5d91a611b29ab819eadc14729f4
Estevão Soares dos Santos <estevao.santos@gmail.com>
2018-10-29 20:02:12 +0000

    Update editor.js

--- js/editor.js
+++ js/editor.js
@@ -428,19 +428,17 @@ window.onload = function () {
     } else {
       $http.get('md/text.md')
         .success(function (data) {
-					$scope.text = data;
-					$http.get('//raw.githubusercontent.com/wiki/showdownjs/showdown/Showdown\'s-Markdown-syntax.md')
-						.success(function (data2) {
-							$scope.text = $scope.text + '\n\n' + data2;
-						});
-        })
-        .error(function () {
-          $scope.text = '';
+          $scope.text = data;
+          $http.get('//raw.githubusercontent.com/wiki/showdownjs/showdown/Showdown\'s-Markdown-syntax.md')
+               .success(function (data2) {
+          $scope.text = $scope.text + '\n\n' + data2;
         });
+      })
+      .error(function () {
+        $scope.text = '';
+      });
     }
-
   }]);
-
 
   angular.bootstrap(document, ['showdown.editor']);
 };
